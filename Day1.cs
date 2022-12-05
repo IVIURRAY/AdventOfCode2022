2031958fe41bed1cd8959aafb3e79f1b12a0ca4b
Haydn Murray <haydn@Haydns-MacBook-Pro.local>
2022-12-05 17:11:59 +0000

    Woah! - I did day 1 in dotnet

---
+++ Day1.cs
@@ -0,0 +1,63 @@
+﻿namespace AdventOfCode2022
+{
+    public class Day1
+	{
+		public Day1()
+		{
+		}
+
+        public static int PartOne()
+        {
+            List<string> data = System.IO.File.ReadAllText(@"/Users/haydn/Documents/Code/AdventOfCode2022/Input/Day1.txt")
+                .Split("\n\n", StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+
+            int maxCalories = 0;
+            foreach (string line in data)
+            {
+                int elfsCalories = line.Split(new string[] { " ", "\n" }, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(x => int.Parse(x))
+                    .ToList()
+                    .Sum();
+
+                if (elfsCalories > maxCalories)
+                {
+                    maxCalories = elfsCalories;
+                }
+            }
+
+            return maxCalories;
+
+        }
+
+        public static int PartTwo()
+        {
+
+            List<string> data = System.IO.File.ReadAllText(@"/Users/haydn/Documents/Code/AdventOfCode2022/Input/Day1.txt")
+                .Split("\n\n", StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+
+            List<int> elfsCalories = new List<int> { };
+            foreach (string line in data)
+            {
+                int elfCalories = line.Split(new string[] { " ", "\n" }, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(x => int.Parse(x))
+                    .ToList()
+                    .Sum();
+
+                elfsCalories.Add(elfCalories);
+            }
+
+            int maxCalories = 0;
+            for (int i = 0; i < 3; i++)
+            {
+                int localMax = elfsCalories.Max();
+                maxCalories += localMax;
+                elfsCalories.Remove(localMax);
+            }
+
+            return maxCalories;
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,4 @@
+﻿using AdventOfCode2022;
+
+Console.WriteLine("Day1 PartOne = " + Day1.PartOne());
+Console.WriteLine("Day1 PartTwo = " + Day1.PartTwo());
