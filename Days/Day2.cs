93a5c555dfa35ea62e3f30a4f549af6968013f41
IVIURRAY <haydn.murray1@yahoo.co.uk>
2022-12-05 18:18:02 +0000

    Day 2 - wahoo!

---
+++ Days/Day2.cs
@@ -0,0 +1,136 @@
+﻿namespace AdventOfCode2022;
+
+public class Day2
+{
+	public Day2()
+	{
+	}
+
+    static int PlayGame(char rps, char choice)
+    {
+        // A = Rock, X = Rock
+        // B = Paper, Y = Paper
+        // C = Scissors, Z = Scissors
+        if (rps == 'A' & choice == 'X')
+        {
+            return 1 + 3;
+        }
+        else if (rps == 'B' & choice == 'Y')
+        {
+            return 2 + 3;
+        }
+        else if (rps == 'C' & choice == 'Z')
+        {
+            return 3 + 3;
+        }
+        else if (rps == 'A' & choice == 'Y')
+        {
+            return 2 + 6;
+        }
+        else if (rps == 'A' & choice == 'Z')
+        {
+            return 3 + 0;
+        }
+        else if (rps == 'B' & choice == 'X')
+        {
+            return 1 + 0;
+        }
+        else if (rps == 'B' & choice == 'Z')
+        {
+            return 3 + 6;
+        }
+        else if (rps == 'C' & choice == 'X')
+        {
+            return 1 + 6;
+        }
+        else if (rps == 'C' & choice == 'Y')
+        {
+            return 2 + 0;
+        }
+        else
+        {
+            return -1000000000;
+        }
+    }
+
+    public static int PartOne()
+	{
+        
+
+        List<string> data = System.IO.File.ReadAllText(@"/Users/haydn/Documents/Code/AdventOfCode2022/Input/Day2.txt")
+			.Split("\n", StringSplitOptions.RemoveEmptyEntries)
+			.ToList();
+
+		int score = 0;
+        foreach (string line in data) {
+            score += PlayGame(line[0], line[2]);
+        }
+
+
+        return score;
+	}
+
+    static int PlayGame2(char rps, char needTo)
+    {
+        // A = Rock, X = Lose
+        // B = Paper, Y = Dawn
+        // C = Scissors, Z = Win
+        if (rps == 'A' & needTo == 'X')
+        {
+            return 3 + 0;
+        }
+        else if (rps == 'A' & needTo == 'Y')
+        {
+            return 1 + 3;
+        }
+        else if (rps == 'A' & needTo == 'Z')
+        {
+            return 2 + 6;
+        }
+        else if (rps == 'B' & needTo == 'X')
+        {
+            return 1 + 0;
+        }
+        else if (rps == 'B' & needTo == 'Y')
+        {
+            return 2 + 3;
+        }
+        else if (rps == 'B' & needTo == 'Z')
+        {
+            return 3 + 6;
+        }
+        else if (rps == 'C' & needTo == 'X')
+        {
+            return 2 + 0;
+        }
+        else if (rps == 'C' & needTo == 'Y')
+        {
+            return 3 + 3;
+        }
+        else if (rps == 'C' & needTo == 'Z')
+        {
+            return 1 + 6;
+        }
+        else
+        {
+            return -1000000000;
+        }
+    }
+
+    public static int PartTwo()
+    {
+        List<string> data = System.IO.File.ReadAllText(@"/Users/haydn/Documents/Code/AdventOfCode2022/Input/Day2.txt")
+            .Split("\n", StringSplitOptions.RemoveEmptyEntries)
+            .ToList();
+
+        int score = 0;
+        foreach (string line in data)
+        {
+            score += PlayGame2(line[0], line[2]);
+        }
+
+
+        return score;
+    }
+
+}
--- Program.cs
+++ Program.cs
@@ -2,3 +2,6 @@
 
 Console.WriteLine("Day1 PartOne = " + Day1.PartOne());
 Console.WriteLine("Day1 PartTwo = " + Day1.PartTwo());
+
+Console.WriteLine("Day2 PartOne = " + Day2.PartOne());
+Console.WriteLine("Day2 PartTwo = " + Day2.PartTwo());
