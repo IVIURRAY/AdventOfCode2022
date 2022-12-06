3533451455838feab2b014e0f8b8e105e9252ef3
IVIURRAY <haydn.murray1@yahoo.co.uk>
2022-12-06 17:19:05 +0000

    adding program main

--- Program.cs
+++ Program.cs
@@ -1,7 +1,14 @@
 ﻿using AdventOfCode2022;
 
-Console.WriteLine("Day1 PartOne = " + Day1.PartOne());
-Console.WriteLine("Day1 PartTwo = " + Day1.PartTwo());
 
-Console.WriteLine("Day2 PartOne = " + Day2.PartOne());
-Console.WriteLine("Day2 PartTwo = " + Day2.PartTwo());
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Day1 PartOne = " + Day1.PartOne()); // 66306
+        Console.WriteLine("Day1 PartTwo = " + Day1.PartTwo()); // 195292
+
+        Console.WriteLine("Day2 PartOne = " + Day2.PartOne()); // 10310
+        Console.WriteLine("Day2 PartTwo = " + Day2.PartTwo()); // 14859
+    }
+}
